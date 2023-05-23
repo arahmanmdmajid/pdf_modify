9b6e0b43998cc5db27f87def65cd358b2f3fc4c4
Abdur Rahman <42006410+arahmanmdmajid@users.noreply.github.com>
2023-05-23 19:17:45 +0300

    Added comment to insert_blank_page.py

--- insert_blank_page.py
+++ insert_blank_page.py
@@ -1,3 +1,6 @@
+# The code below adds a blank page after every page in the PDF file
+
+#Before running the code 'pip install PyPDF2' module from this link https://pypi.org/project/PyPDF2/
 from PyPDF2 import PdfReader, PdfWriter
 
 dr = r"C:\Docs"
@@ -5,14 +8,16 @@ ldr = dr + r"\original.pdf"
 writer = PdfWriter()
 
 with open(ldr, "rb") as f:
-	#suply file to reader
+	#supply file to reader
 	reader = PdfReader(f)
 	
+	# get the total number for pages
 	number_of_pages = len(reader.pages)
 	
 	for i in range(number_of_pages):
 		page = reader.pages[i]
 		writer.add_page(page)
+		# add blank page after each page
 		writer.add_blank_page(page)
   
 		with open(dr + r"\new.pdf", "wb") as output_stream:
